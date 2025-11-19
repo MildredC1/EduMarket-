9f626bbfb13aa54f5ba0a22e94240186b86e0304
Mildred <copmildred@gmail.com>
2025-11-18 23:39:47 -0600

    Creacion de las rutas de registro, login y logout

---
+++ src/routes/auth.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { registrarse, iniciarSesion } from '../controllers/authController.js';
+
+const router = express.Router();
+
+// Registro
+router.post('/registro',registrarse);
+
+// Login
+router.post('/login', iniciarSesion);
+
+// Logout
+router.post('/logout', (req, res) => {
+  res.clearCookie('usuario');
+  res.clearCookie('rol');
+  res.json({ mensaje: 'Sesión cerrada correctamente' });
+});
+
+export default router;

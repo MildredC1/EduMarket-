df14647a5e99e82c322a3a20133bdf63217574fa
Mildred <copmildred@gmail.com>
2025-11-18 22:26:28 -0600

    se agrego el authController con las autenticaciones respectivas ya que se envio vacio

---
+++ src/controllers/authController.js
@@ -0,0 +1,45 @@
+import pool from '../config/db.js';
+import bcrypt from 'bcrypt';
+
+export async function registrarse(req, res) {
+  const { nombre, apellido, correo, contrasena } = req.body;
+
+  if (!nombre || !apellido || !correo || !contrasena) {
+    return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+  }
+
+  const [existe] = await pool.promise().query(
+    'SELECT id FROM usuarios WHERE correo = ?',
+    [correo]
+  );
+  if (existe.length > 0) {
+    return res.status(400).json({ error: 'Correo ya registrado' });
+  }
+
+  const hash = await bcrypt.hash(contrasena, 10);
+  await pool.promise().query(
+    'INSERT INTO usuarios (nombre, apellido, correo, contrasena_hash, rol) VALUES (?, ?, ?, ?, ?)',
+    [nombre, apellido, correo, hash, 'estudiante']
+  );
+
+  res.status(201).json({ mensaje: 'Usuario registrado con éxito' });
+}
+
+export async function iniciarSesion(req, res) {
+  const { correo, contrasena } = req.body;
+
+  const [[usuario]] = await pool.promise().query(
+    'SELECT * FROM usuarios WHERE correo = ?',
+    [correo]
+  );
+  if (!usuario) return res.status(401).json({ error: 'Usuario no encontrado' });
+
+  const valido = await bcrypt.compare(contrasena, usuario.contrasena_hash);
+  if (!valido) return res.status(401).json({ error: 'Contraseña incorrecta' });
+
+  
+  res.cookie('usuario', usuario.nombre, { httpOnly: true, maxAge: 3600000 });
+  res.cookie('rol', usuario.rol, { httpOnly: true, maxAge: 3600000 });
+
+  res.json({ mensaje: 'Login exitoso', usuario: { id: usuario.id, nombre: usuario.nombre, rol: usuario.rol } });
+}

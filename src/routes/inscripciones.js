87ba4b964a2cdfb2a3c6e638eefac4002804a353
Mildred Cop <copmildred@gmail.com>
2025-11-19 09:29:49 -0600

    Se modifico las inscripciones routes para que pase el filtro de roles e inicios de sesion

--- src/routes/inscripciones.js
+++ src/routes/inscripciones.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { inscribirseEnCurso, listarCursosInscritos } from '../controllers/inscripcionesController.js';
+import { verificarSesion } from '../middlewares/sesion.js';
+import { soloEstudiante } from '../middlewares/roles.js';
 
 const router = express.Router();
 
-// Inscribirse en curso
-router.post('/cursos/:id/inscribirse', inscribirseEnCurso);
+// Inscribirse en curso (solo estudiantes con sesión)
+router.post('/cursos/:id/inscribirse', verificarSesion, soloEstudiante, inscribirseEnCurso);
 
-// Ver cursos inscritos
-router.get('/estudiante/cursos', listarCursosInscritos);
+// Ver cursos inscritos (solo estudiantes con sesión)
+router.get('/estudiante/cursos', verificarSesion, soloEstudiante, listarCursosInscritos);
 
 export default router;

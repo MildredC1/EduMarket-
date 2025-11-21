8208f4ae1251b1332fdac18e8ca29775003b002f
MildredC1 <copmildred@gmail.com>
2025-11-21 04:14:54 -0600

    Se implemento rutas para inscribirse y mis cursos

--- src/routes/cursos.js
+++ src/routes/cursos.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import { listarCursos, crearCurso, verDetalleCurso,actualizarCurso,listarCursosInstructor } from '../controllers/cursosController.js';
+import { listarCursos, crearCurso, verDetalleCurso,actualizarCurso,listarCursosInstructor,listarMisCursos,inscribirseCurso } from '../controllers/cursosController.js';
 import { verificarSesion } from '../middlewares/sesion.js';
-import { soloAdminOInstructor, soloInstructor } from '../middlewares/roles.js';
+import { soloAdminOInstructor, soloInstructor ,soloEstudiante} from '../middlewares/roles.js';
 
 const router = express.Router();
 
@@ -17,5 +17,8 @@ router.put('/:id', verificarSesion, soloAdminOInstructor, actualizarCurso);
 
 // Ver cursos creados por el instructor logueado
 router.get('/mis-cursos', verificarSesion, soloAdminOInstructor, listarCursosInstructor);
+
+router.post('/:id/inscribirse', verificarSesion, soloEstudiante, inscribirseCurso);
+router.get('/mis-cursos-estudiante', verificarSesion, soloEstudiante, listarMisCursos);
 
 export default router;

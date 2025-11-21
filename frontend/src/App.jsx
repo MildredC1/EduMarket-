8c330787f9e78132b981855ac4ffafb0d7cb940e
MildredC1 <copmildred@gmail.com>
2025-11-21 03:42:39 -0600

    Se configuraron las rutas para los componentes de detallecurso y miscursos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import Cursos from './pages/Cursos.jsx';
 import Registro from './pages/Registro.jsx';
 import Login from './pages/Login.jsx';
 import CrearCurso from './pages/CrearCurso';
+import DetalleCurso from './pages/DetalleCurso';
+import MisCursos from './pages/MisCursos'; 
 
 
 
@@ -19,6 +21,8 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/cursos" element={<Cursos />} />
         <Route path="/crear-curso" element={<CrearCurso />} />
+        <Route path="/curso/:id" element={<DetalleCurso />} />
+        <Route path="/mis-cursos" element={<MisCursos />} />
       </Routes>
     </BrowserRouter>
   );

4c9da54905c976f4eda3c87a7d618496c5b50903
MildredC1 <copmildred@gmail.com>
2025-11-23 19:36:09 -0600

    Se agrego importacion para editarCurso

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import CrearCurso from './pages/CrearCurso';
 import DetalleCurso from './pages/DetalleCurso';
 import MisCursos from './pages/MisCursos.jsx';
 import GestionUsuarios from './pages/GestionUsuarios.jsx';
+import EditarCurso from './pages/EditarCurso.jsx';
 
 
 export default function App() {
@@ -21,9 +22,11 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/cursos" element={<Cursos />} />
         <Route path="/crear-curso" element={<CrearCurso />} />
+        <Route path="/editar-curso/:id" element={<EditarCurso />} />
         <Route path="/curso/:id" element={<DetalleCurso />} />
         <Route path="/mis-cursos" element={<MisCursos />} />
         <Route path="/gestion-usuarios" element={<GestionUsuarios />} />
+        
       </Routes>
     </BrowserRouter>
   );
